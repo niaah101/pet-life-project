d293aaf4ff677c551c787b440ff562711c55dd60
Denia Davis <niaah101@yahoo.com>
2020-07-02 23:55:44 -0400

    Color blocked div for layout view

--- toggle.js
+++ toggle.js
@@ -1,14 +1,25 @@
 // Toggle for hamburger menu
 
-$(function(){
+//$(function(){
 
-    $(".toggle").on("click",function(){
+    //$(".toggle").on("click",function(){
 
-        if($(".item").hasClass("active")){
-            $(".item").removeClass("active");
-        }
-        else{
-            $(".item").addClass("active");
-        }
-    })
-});
+        //if($(".item").hasClass("active")){
+           // $(".item").removeClass("active");
+       // }
+       // else{
+           // $(".item").addClass("active");
+       // }
+    //})
+//});
+
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
